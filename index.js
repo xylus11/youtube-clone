e282c5a2271c07214ec83b8732eee74b30e96fb4
mangeshakaxylus11 <Mangeshahire78@gmail.com>
2024-02-01 18:29:46 +0530

    added css html assets

---
+++ index.js
@@ -0,0 +1,4 @@
+const videoDiv = document.getElementById('video-section');
+const div_1=document.createElement('div');
+const div_2=document.createElement('div');
+const div_3=document.createElement('div');
