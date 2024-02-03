6be87b14cd9e3502450f973621e1450d080e13d3
mangeshakaxylus11 <Mangeshahire78@gmail.com>
2024-02-03 18:12:21 +0530

    added css html assets

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-API_KEY="AIzaSyBqEL3dL2nstFjPKh-jI7uwdOjCxkoszKo"
+API_KEY="AIzaSyCFl3FSpqHJOxzWyIOFNgv0PnHYxMWwt8E"
 BASE_URL="https://www.googleapis.com/youtube/v3"
 
 const btn = document.getElementById('button');
@@ -23,8 +23,8 @@ function dispVideos(videos)
        <a class='anc' href="watch.html?videoId=${video.id.videoId}" >
      <img src=" ${video.snippet.thumbnails.medium.url} " width:"20px"/>
   
-       <div class='divide'>
-       
+       <div id='divide' >
+    
       <div class='video-title'>
       ${video.snippet.title}
       </div>
@@ -32,11 +32,14 @@ function dispVideos(videos)
       ${video.snippet.channelTitle}
       
     
+     
       </div>
-       <div>
+      <div>
       </a>
       
-      </div> `
+      </div> `;
+
+    //   loadChannelInfo(video.snippet.channelId);
         
     })
 }
@@ -46,15 +49,46 @@ getVideo('krsna - nocap')
 
  async function getVideo(searchKey)
 { 
-    const response = await fetch(`${BASE_URL}/search?key=${API_KEY}&q=${searchKey}&type=video&maxResults=20&part=snippet`)
+    const response = await fetch(`${BASE_URL}/search?key=${API_KEY}&q=${searchKey}&type=video&maxResults=2&part=snippet`)
     const data = await response.json();
         console.log(data.items);
-        dispVideos(data.items)
+      
+        
+        dispVideos(data.items);
+        // if (data.items.length > 0) {
+        //     const channelId = data.items[0].snippet.channelId;
+        //     loadChannelInfo(channelId);
+        // }
     }
 
 
+    // async function loadChannelInfo(channelId) {
+    //     try {
+    //         const response = await fetch(`${BASE_URL}/channels?key=${API_KEY}&part=snippet&id=${channelId}`);
+    //         if (!response.ok) {
+    //             throw new Error(`HTTP error! status: ${response.status}`);
+    //         }
+    //         const data = await response.json();
+    //         if (data.items) {
+    //             displayChannelPic(data.items[0]);
+               
+    //         }
+    //     } catch (error) {
+    //         console.error('Error fetching channel info: ', error);
+    //     }
+    // }
+
+    // function displayChannelPic(channelData) {
+    //     const channelInfoSection = document.getElementById('divide');
+    //     channelInfoSection.innerHTML = `
+         
+    //          <img src="${channelData.snippet.thumbnails.default.url}" alt="${channelData.snippet.title}">
 
 
+          
+    //     `;
+        
+    // }
 
 
 // //////////////////////////////////////////////////////////////////////
--- watch.js
+++ watch.js
@@ -1,35 +1,156 @@
+API_KEY="AIzaSyCFl3FSpqHJOxzWyIOFNgv0PnHYxMWwt8E"
+BASE_URL="https://www.googleapis.com/youtube/v3"
+
+
 const search = window.location.search;
 const params = new URLSearchParams(search);
 const videoId = params.get('videoId');
 const container = document.getElementById('video-container');
+const title = document.getElementById('title');
+const desc = document.getElementById('desc');
 
 
     container.innerHTML=`
-    <iframe id='frame' width="920" height="460" src="https://www.youtube.com/embed/${videoId}"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+    <iframe id='frame' width="920" height="480" src="https://www.youtube.com/embed/${videoId}" 
+     frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+      picture-in-picture; web-share" allowfullscreen></iframe>
+      `
+    function videoTitle(titleName,description)
+    {
+          title.innerHTML=`<h3>${titleName}</h3>`
+          desc.innerHTML=`<p>${description}</p>`
 
-const profile =document.getElementById('recom-watch');
-profile.innerHTML=`<p>
+    }
+   
 
 
 
+    const profile =document.getElementById('recom-watch');
+    loadVideoDetails(videoId); 
+  
+    async function loadVideoDetails(videoId) {
+        try {
+            const response = await fetch(`${BASE_URL}/videos?key=${API_KEY}&part=snippet&id=${videoId}`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log(data.items);
+            if (data.items && data.items.length > 0) {
+                const channelId = data.items[0].snippet.channelId;
+                loadChannelInfo(channelId);
+            }
+        } catch (error) {
+            console.error('Error fetching video details: ', error);
+        }
+    }
+    
+    async function loadChannelInfo(channelId) {
+        try {
+            const response = await fetch(`${BASE_URL}/channels?key=${API_KEY}&part=snippet&id=${channelId}`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            if (data.items) {
+                displayChannelInfo(data.items[0]);
+                loadRecommendedVideos(data.items[0].snippet.title);
+            }
+        } catch (error) {
+            console.error('Error fetching channel info: ', error);
+        }
+    }
+    
+    function displayChannelInfo(channelData) {
+        const channelInfoSection = document.getElementById('channel-info');
+        channelInfoSection.innerHTML = `
+             <div>
+            <img src="${channelData.snippet.thumbnails.default.url}" alt="${channelData.snippet.title}">
+            <h3>${channelData.snippet.title}</h3>
+
+           </div>
+        `;
+        
+    }
 
 
+    // Make a request to the YouTube API to get video details
+    fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${API_KEY}&part=snippet`)
+        .then(response => response.json())
+        .then(data => {
+            const description=data.items[0].snippet.description;
+            // Extract the video name from the API response
+            const videoName = data.items[0].snippet.title;
+            console.log('Video Name:', videoName);
+             return videoTitle(videoName,description);
+        })
+        .catch(error => console.error('Error fetching video details:', error));
 
-
-
-fsd
-fs
-df
-
-fs
-f
-
-f
-sd
-fd
-fd
-d
-f
-df
-adadaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-</p>`
+    // async function loadComments(videoId) {
+    //     try {
+    //         const response = await fetch(`${BASE_URL}/commentThreads?key=${API_KEY}&videoId=${videoId}&maxResults=25&part=snippet`);
+    //         if (!response.ok) {
+    //             throw new Error(`HTTP error! status: ${response.status}`);
+    //         }
+    //         const data = await response.json();
+    //         console.log("comments", data)
+    //         if (data.items) {
+    //             displayComments(data.items);
+    //         } else {
+    //             console.log("No comments available or data is undefined.");
+    //         }
+    //     } catch (error) {
+    //         console.error('Error fetching comments: ', error);
+    //     }
+    // }
+    
+    // function displayComments(comments) {
+    //     const commentSection = document.getElementById('comment-section');
+    //     commentSection.innerHTML = '';
+    
+    //     comments.forEach(comment => {
+    //         const commentText = comment.snippet.topLevelComment.snippet.textDisplay;
+    //         const commentElement = document.createElement('p');
+    //         commentElement.innerHTML = commentText;
+    //         commentSection.appendChild(commentElement);
+    //     });
+    // }
+    
+    async function loadRecommendedVideos(channelName) {
+        try {
+            const response = await fetch(`${BASE_URL}/search?key=${API_KEY}&maxResults=8&part=snippet&q=${channelName}`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log("Recommended videos", data)
+            if (data.items) {
+                displayRecommendedVideos(data.items);
+            } else {
+                console.log("No recommended videos available or data is undefined.");
+            }
+        } catch (error) {
+            console.error('Error fetching recommended videos: ', error);
+        }
+    }
+    
+    function displayRecommendedVideos(videos) {
+        const recommendedSection = document.getElementById('recom-watch');
+        recommendedSection.innerHTML = '';
+    
+        videos.forEach(video => {
+            const videoId = video.id.videoId;
+            const title = video.snippet.title;
+            const thumbnail = video.snippet.thumbnails.default.url;
+            const videoCard = document.createElement('div');
+            videoCard.className='recom-video';
+            videoCard.innerHTML = `
+                <a href="watch.html?videoId=${videoId}">
+                    <img src="${thumbnail}" alt="${title}">
+                    <p>${title}</p>
+                </a>
+            `;
+            recommendedSection.appendChild(videoCard);
+        });
+    }
+    
